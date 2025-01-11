776d1063f7543f3b5d5d41517874fc2ba10dcf97
Ankush Thakur <ankusht2307@gmail.com>
2025-01-11 06:35:21 +0530

    Added BS

--- search/linear-search.js
+++ search/linear-search.js
@@ -1,9 +1,9 @@
 /**
- * Problem: Given an array if 'n' elements and a target element 't', find the index of 't'
+ * Problem: Given an array of 'n' elements and a target element 't'. Find the index of 't'
  * in the array. Return -1 if the target element is not found.
  *
  * arr = [-5, 2, 10, 4, 6], t = 10 => should return 2
- * arr = [-5, 2, 10, 4, 6], t = 6 =< should return 4
+ * arr = [-5, 2, 10, 4, 6], t = 6 => should return 4
  *
  */
 
