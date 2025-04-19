0bf1515f61da176aa0332fe3952e04825300bcaa
Ankush Thakur <ankusht2307@gmail.com>
2025-04-19 22:18:21 +0530

    Added Questions

--- search/binary-search/order-agnostic-bs.js
+++ search/binary-search/order-agnostic-bs.js
@@ -18,26 +18,26 @@
 
 const orderAgnosticBS = (arr, target) => {
   if (arr && arr.length < 1) return -1;
-  let left = 0;
-  let right = arr.length - 1;
-  const isAscending = arr[left] < arr[right];
+  let start = 0;
+  let end = arr.length - 1;
+  const isAscending = arr[start] < arr[end];
 
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
 
     if (arr[mid] === target) return mid;
 
     if (isAscending) {
       if (arr[mid] < target) {
-        left = mid + 1;
+        start = mid + 1;
       } else {
-        right = mid - 1;
+        end = mid - 1;
       }
     } else {
       if (arr[mid] < target) {
-        right = mid - 1;
+        end = mid - 1;
       } else {
-        left = mid + 1;
+        start = mid + 1;
       }
     }
   }
--- search/binary-search/questions/findPeakInMountainArray.js
+++ search/binary-search/questions/findPeakInMountainArray.js
@@ -1,3 +1,17 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+/**
+ * Problem statement: You are given an integer mountain array arr of length n 
+ * where the values increase to a peak element and then decrease.
+ * 
+ * Return the index of the peak element.
+ *
+ * Example:
+ * Input: arr = [0,1,0]
+ * Output: 1
+ *
+ */
+
 const bruteFindPeak = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > arr[i + 1]) {
